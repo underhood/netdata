fdbd61bcb0ae1394d8389c6decc90b93fb46189f
Emmanuel Vasilakis <mrzammler@mm.st>
2021-06-28 11:41:43 +0300

    cleanups

--- database/rrdhost.c
+++ database/rrdhost.c
@@ -316,80 +316,18 @@ RRDHOST *rrdhost_create(const char *hostname,
                 int rc = sql_create_health_log_table(host);
                 if (unlikely(rc)) {
                     error_report("Failed to create health log table in the database");
-                    //mark that we dont have a table, and continue with file health.db
-                    //there is a check if the file is open when attempting to write entries
+
                     health_alarm_log_load(host);
                     health_alarm_log_open(host);
                 }
                 else {
                     health_alarm_log_load(host);
                     add_migrated_file(host->health_log_filename, 0);
-                    //health_alarm_log_open(host);
-
-                    //dump debug
-                    /* int count=0; */
-                    /* unsigned int max = host->health_log.max; */
-                    /* BUFFER *wb = buffer_create(10000); */
-                    /* /\* ALARM_ENTRY *ae; *\/ */
-                    /* /\* for (ae = host->health_log.alarms; ae && count < max; ae = ae->next) { *\/ */
-                    /* /\*     if (likely(count)) *\/ */
-                    /* /\*         buffer_strcat(wb, ","); *\/ */
-                    /* /\*     health_alarm_entry_sql2json(wb, ae->unique_id, ae->alarm_id, host); *\/ */
-                    /* /\*     count++; *\/ */
-                    /* /\* } *\/ */
-
-                    /* health_alarm_log2json(host, wb, 0, NULL); */
-                    /* error_report("health_log.alarms has [%d]", count); */
-
-                    /* //error_report(buffer_tostring(wb)); */
-                    /* char *lala; */
-                    /* lala = strdupz(buffer_tostring(wb)); */
-
-                    /* FILE *fp_loge = fopen("/tmp/alarm_log_from_file", "w"); */
-                    /* fprintf(fp_loge, "%s", lala); */
-                    /* fclose(fp_loge); */
-
-                    /* //do the same from sqlite */
-                    /* BUFFER *wb2 = buffer_create(10000); */
-                    /* sql_health_alarm_log_select_all(wb2, host); */
-                    /* char *lala2; */
-                    /* lala2 = strdupz(buffer_tostring(wb2)); */
-
-                    /* FILE *fp_loge2 = fopen("/tmp/alarm_log_sql", "w"); */
-                    /* fprintf(fp_loge2, "%s", lala2); */
-                    /* fclose(fp_loge2); */
                 }
             } else {
                 sql_health_alarm_log_load(host);
-                //health_alarm_log_open(host);
-
-                /* int count=0; */
-                /* unsigned int max = host->health_log.max; */
-                /* BUFFER *wb = buffer_create(10000); */
-                /* /\* ALARM_ENTRY *ae; *\/ */
-                /* /\* for (ae = host->health_log.alarms; ae && count < max; ae = ae->next) { *\/ */
-                /* /\*     if (likely(count)) *\/ */
-                /* /\*         buffer_strcat(wb, ","); *\/ */
-                /* /\*     health_alarm_entry_sql2json(wb, ae->unique_id, ae->alarm_id, host); *\/ */
-                /* /\*     count++; *\/ */
-                /* /\* } *\/ */
-
-                /* health_alarm_log2json(host, wb, 0, NULL); */
-                /* error_report("health_log.alarms has [%d]", count); */
-
-                /* //error_report(buffer_tostring(wb)); */
-                /* char *lala; */
-                /* lala = strdupz(buffer_tostring(wb)); */
-
-                /* FILE *fp_loge = fopen("/tmp/alarm_log_loaded_from_sql", "w"); */
-                /* fprintf(fp_loge, "%s", lala); */
-                /* fclose(fp_loge); */
-                
             }
-
-            
         }
-        
     }
     else
         error_report("Host machine GUID %s is not valid", host->machine_guid);
@@ -587,8 +525,7 @@ void rrdhost_update(RRDHOST *host
                 int rc = sql_create_health_log_table(host);
                 if (unlikely(rc)) {
                     error_report("Failed to create health log table in the database");
-                    //mark that we dont have a table, and continue with file health.db
-                    //there is a check if the health log file is open when saving entries
+
                     health_alarm_log_load(host);
                     health_alarm_log_open(host);
                 } else {
